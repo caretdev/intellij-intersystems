5667fcd8e2585892352f47254a29dba3d975a7bb
GitHub Action <action@github.com>
2021-03-23 13:39:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/daimor/intellijintersystems/MyBundle.kt
+++ src/main/kotlin/com/github/daimor/intellijintersystems/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.daimor.intellijintersystems
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/daimor/intellijintersystems/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.daimor.intellijintersystems.services
+
+import com.github.daimor.intellijintersystems.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
