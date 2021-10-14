9cc41a3fd2163f80d56aeed486d597751ec3ec34
Dmitry Maslennikov <mrdaimor@gmail.com>
2021-10-14 22:11:27 +0300

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,17 +8,17 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.5.10"
+    id("org.jetbrains.kotlin.jvm") version "1.5.31"
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-    id("org.jetbrains.intellij") version "0.7.3"
+    id("org.jetbrains.intellij") version "1.2.0"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
-    id("org.jetbrains.changelog") version "1.1.2"
+    id("org.jetbrains.changelog") version "1.3.1"
     // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
-    id("io.gitlab.arturbosch.detekt") version "1.17.1"
+    id("io.gitlab.arturbosch.detekt") version "1.18.1"
     // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
-    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
 
-    id("de.undercouch.download") version "4.1.1"
+    id("de.undercouch.download") version "4.1.2"
 }
 
 group = properties("pluginGroup")
@@ -31,7 +31,7 @@ repositories {
     maven("https://jitpack.io")
 }
 dependencies {
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.17.1")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.18.1")
     implementation("com.github.ballerina-platform:lsp4intellij:master-SNAPSHOT")
 }
 
@@ -70,7 +70,6 @@ detekt {
 
 tasks.withType<KotlinCompile>().configureEach {
     kotlinOptions {
-        useIR = true
     }
 }
 
